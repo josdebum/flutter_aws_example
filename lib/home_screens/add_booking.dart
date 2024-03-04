acf4f0f2ee4a02462dcad37b7224061ca9fa3972
Joseph Deborah Oluwabunmi <37649534+josdebum@users.noreply.github.com>
2024-03-04 14:56:21 +0100

    added pick image feature

--- lib/home_screens/add_booking.dart
+++ lib/home_screens/add_booking.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_aws_project/widgets/app_textfield.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_aws_project/widgets/extensions.dart';
 import 'package:flutter_aws_project/widgets/home_aws.dart';
 import 'package:flutter_aws_project/widgets/size_config.dart';
 import 'package:flutter_aws_project/widgets/widget_extension.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -25,6 +28,24 @@ class AddNewScreenState extends State<AddNewScreen> {
   final dateCollected = TextEditingController();
   final dueDate = TextEditingController();
   final homeRepo = HomeRepo();
+  Uint8List? _imageFile1;
+  Uint8List? _imageFile2;
+  Uint8List? _imageFile3;
+  Uint8List? _imageFile4;
+  Future<void> _pickImage(int number) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    Uint8List imageFile = await file!.readAsBytes();
+    number == 1
+        ? _imageFile1 = imageFile
+        : number == 2
+        ? _imageFile2 = imageFile: number == 3?
+         _imageFile3 = imageFile : _imageFile4 = imageFile;
+
+    setState(() {
+      itemImage(number);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
@@ -35,12 +56,49 @@ class AddNewScreenState extends State<AddNewScreen> {
         padding:   EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
     child: Column(children: <Widget>[
 
-        DottedBorder(
-        color: kRed, //color of dotted/dash line
-        strokeWidth: 1, //thickness of dash/dots
-        dashPattern: const [8, 6],
-        borderType: BorderType.Rect,
-        child:   Container(width: double.infinity, height: 200, color: kGrey4)),
+        // DottedBorder(
+        // color: kRed, //color of dotted/dash line
+        // strokeWidth: 1, //thickness of dash/dots
+        // dashPattern: const [8, 6],
+        // borderType: BorderType.Rect,
+        // child:   Container(width: double.infinity, height: 200, color: kGrey4)),
+
+
+            DottedBorder(
+                color: kRed, //color of dotted/dash line
+                strokeWidth: 1, //thickness of dash/dots
+                dashPattern: const [8, 6],
+                borderType: BorderType.Rect,
+                child: Container(
+        height: 300.h,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.0,
+            color:
+            Theme.of(context).brightness == Brightness.dark
+                ? kWhite
+                : kGrey,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: _imageFile1 == null
+                ? MainAxisAlignment.center
+                : MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(children: <Widget>[  itemImage(1),
+                itemImage(2).visible(_imageFile1 != null),]),
+
+                  Row(children: <Widget>[
+              itemImage(3).visible(_imageFile2 != null),
+              itemImage(4).visible(_imageFile3 != null)])
+            ],
+          ),
+        ),
+
+        )).addHeight(20),
           AppTextField(title: "Enter client full name", controller: fullName,).addHeight(20.h),
           AppTextField(title: "Enter email address", controller: emailAddress,).addHeight(20.h),
           AppTextField(title: "Enter phone number", controller: phoneNumber,).addHeight(20.h),
@@ -188,4 +246,38 @@ class AddNewScreenState extends State<AddNewScreen> {
           },
           hasBorder: false).addHeight(40.h),
 
-]))))));}}
+]))))));}
+
+  Widget itemImage(int number) {
+    Uint8List? image = number == 1
+        ? _imageFile1
+        : number == 2
+        ? _imageFile2 : number == 3?
+         _imageFile3 :  _imageFile4;
+    return GestureDetector(
+      onTap: () => _pickImage(number),
+      child: image != null
+          ? Container(
+        height: 100.h,
+        width: MediaQuery.of(context).size.width/2,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          image: DecorationImage(
+            image: MemoryImage(image),
+            fit: BoxFit.fill,
+          ),
+        ),
+      )
+          : Container(
+        height: 100.h,
+        width: 100.w,
+        padding: const EdgeInsets.all(10),
+        child: CircleAvatar(
+          radius: 48,
+          child: Icon(Icons.camera_alt)
+        ),
+      ),
+    );
+  }
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import amplify_secure_storage
 import device_info_plus
+import file_selector_macos
 import package_info_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
